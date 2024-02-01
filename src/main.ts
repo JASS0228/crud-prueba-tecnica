1fda2aab2983a0eb4d75cb6771a861a45b028a36
JASS0228 <Salazarshome@outlook.com>
2024-02-01 13:43:42 -0600

    Se usa sweetalert para toda la aplicacion

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import App from './App.vue'
 import router from './routes'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(VueQueryPlugin)
+app.use(VueSweetalert2)
 app.use(router)
 app.mount('#app')
