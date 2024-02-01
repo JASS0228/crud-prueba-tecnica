5d44c874a1e7e220842e7a6d363f1d69b4a54798
JASS0228 <Salazarshome@outlook.com>
2024-01-31 22:30:16 -0600

    Se cambio el hover de los links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@
 				<RouterLink
 					:to="{ name: 'Hotels' }"
 					class="text-2xl font-bold text-gray-800 hover:text-gray-400 transition-all"
-					exact-active-class="underline text-gray-400"
+					exact-active-class="underline"
 					>Hotels</RouterLink
 				>
 			</li>
@@ -18,7 +18,7 @@
 				<RouterLink
 					:to="{ name: 'crear-hotel' }"
 					class="text-2xl font-semibold text-gray-800 hover:text-gray-400 transition-all"
-					exact-active-class="underline text-gray-400"
+					exact-active-class="underline"
 					>Crear Hotel</RouterLink
 				>
 			</li>
