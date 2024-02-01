716d5e7fde57aba0ec527b72e51298270f5cc1b5
JASS0228 <Salazarshome@outlook.com>
2024-02-01 01:28:17 -0600

    Se explica el porque se creo un cliente de axios

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+//Este cliente es para no tener que estar escribiendo la url base cada vez que hagamos una petición
 const hotelsApi = axios.create({
 	baseURL: 'https://test.devmx.site/api',
 })
