96ed2622fff17aa0593f9d4faee231da89d600c5
JASS0228 <Salazarshome@outlook.com>
2024-01-31 20:37:47 -0600

    Se agrego las rutas para tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './index.html'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-
