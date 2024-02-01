ac4daea6e89d8a44687628082f2f38a6e0873818
JASS0228 <Salazarshome@outlook.com>
2024-01-31 21:46:02 -0600

    Se creo un componente card para poner la información de cada Hotel

---
+++ src/components/HotelCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+	<article class="bg-white rounded-md shadow-md">
+		<div>
+			<img
+				src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/20/c4/5a/5a/hotel-safi-metropolitan.jpg?w=1200&h=-1&s=1%201x,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/20/c4/5a/5a/hotel-safi-metropolitan.jpg?w=1400&h=-1&s=1%202x"
+				alt=""
+				class="rounded shadow" />
+		</div>
+		<div class="m-3">
+			<div class="flex items-center justify-between">
+				<h2 class="text-2xl font-bold">Hotel Safi Metropolitan</h2>
+				<!-- Todo agregar icono -->
+				<p class="text-gray-500">Rate</p>
+			</div>
+			<div>
+				<p class="text-gray-500">Apodaca Nuevo leon</p>
+
+				<p class="truncate mt-2">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+					deserunt libero ab corporis illum dolore aliquam eaque sed dolores
+					facilis et fuga repudiandae, ipsam itaque minima quia laboriosam.
+					Maxime, quasi.
+				</p>
+			</div>
+			<div class="flex justify-end items-center gap-1 mt-7">
+				<p class="border-r-2 border-black pr-2">3000</p>
+				<p>USD</p>
+			</div>
+		</div>
+	</article>
+</template>
