f61d535bfcef8ad57f4a851f4f44f0c717bab01e
JASS0228 <Salazarshome@outlook.com>
2024-01-31 20:57:17 -0600

    Se importo la navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <script setup lang="ts">
-	import { RouterView, RouterLink } from 'vue-router'
+	import { RouterView } from 'vue-router'
+	import NavBar from './components/Navbar.vue'
 </script>
 
 <template>
 	<header class="bg-white p-5 shadow-md mb-5">
-		<nav>
-			<ul
-				class="flex flex-col items-center justify-around md:flex-row md:items-center md:justify-start md:gap-6">
-				<li>
-					<RouterLink
-						:to="{ name: 'Hotels' }"
-						class="text-4xl font-bold text-gray-800 hover:text-gray-500 transition-all"
-						exact-active-class="underline text-gray-500"
-						>Hotels</RouterLink
-					>
-				</li>
-				<li>
-					<RouterLink
-						:to="{ name: 'crear-hotel' }"
-						class="text-4xl font-semibold text-gray-600 hover:text-gray-500 transition-all"
-						exact-active-class="underline text-gray-500"
-						>Crear Hotel</RouterLink
-					>
-				</li>
-			</ul>
-		</nav>
+		<NavBar />
 	</header>
 	<main>
 		<RouterView />
