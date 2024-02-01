e64ca9c5cb9e1b36a796a2df902d73af000be12c
JASS0228 <Salazarshome@outlook.com>
2024-02-01 13:45:04 -0600

    Se creo la ruta del borrar el hotel por el id y se agrego información sobre la función

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,8 +6,14 @@ const getHotels = async () => {
 	return await hotelsApi.get('/hotels')
 }
 
+// Obtiene un hotel de la API por su id
 const getHotel = async (id: number) => {
 	return await hotelsApi.get<HotelByID>(`/hotels/${id}`)
 }
 
-export { getHotels, getHotel }
+// Elimina un hotel de la API por su id
+const deleteHotelById = async (id: number) => {
+	return await hotelsApi.delete(`/hotels/${id}`)
+}
+
+export { getHotels, getHotel, deleteHotelById }
