d42095898c6537b345b2bd794d5fa9acd8c22d06
JASS0228 <Salazarshome@outlook.com>
2024-02-01 04:38:31 -0600

    Agregamos la ruta para obtener por id el hotel

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,13 @@
 import hotelsApi from '../Config'
+import { Hotel, HotelByID, Hotels } from '../types/hotelType'
 
 // Obtiene todo los hoteles de la APi
 const getHotels = async () => {
 	return await hotelsApi.get('/hotels')
 }
 
-export { getHotels }
+const getHotel = async (id: number) => {
+	return await hotelsApi.get<HotelByID>(`/hotels/${id}`)
+}
+
+export { getHotels, getHotel }
