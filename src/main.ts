7eb84eedbb669f013cf8a848e5e095924de4d757
JASS0228 <Salazarshome@outlook.com>
2024-01-31 23:36:02 -0600

    Se uso tanStack

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import './style.css'
 import App from './App.vue'
 import router from './routes'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
+app.use(VueQueryPlugin)
 app.use(router)
 app.mount('#app')
