7a6847dd1a13844c3430821abf78ccb66f61473c
JASS0228 <Salazarshome@outlook.com>
2024-01-31 20:54:18 -0600

    Se creo una navbar para navegar la ruta de crear y listado

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
-	import { RouterView } from 'vue-router'
+	import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <template>
+	<header class="bg-white p-5 shadow-md mb-5">
+		<nav>
+			<ul
+				class="flex flex-col items-center justify-around md:flex-row md:items-center md:justify-start md:gap-6">
+				<li>
+					<RouterLink
+						:to="{ name: 'Hotels' }"
+						class="text-4xl font-bold text-gray-800 hover:text-gray-500 transition-all"
+						exact-active-class="underline text-gray-500"
+						>Hotels</RouterLink
+					>
+				</li>
+				<li>
+					<RouterLink
+						:to="{ name: 'crear-hotel' }"
+						class="text-4xl font-semibold text-gray-600 hover:text-gray-500 transition-all"
+						exact-active-class="underline text-gray-500"
+						>Crear Hotel</RouterLink
+					>
+				</li>
+			</ul>
+		</nav>
+	</header>
 	<main>
 		<RouterView />
 	</main>
