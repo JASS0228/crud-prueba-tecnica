973a099cc3c776549445d10e5bf7793be70c6958
JASS0228 <Salazarshome@outlook.com>
2024-01-31 21:44:13 -0600

    Se agrego un Listado de HotelCard

--- src/Views/HotelsView.vue
+++ src/Views/HotelsView.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import HotelCard from '../components/HotelCard.vue'
+</script>
 
 <template>
-	<div>Listado de Hoteles</div>
+	<div>
+		<h1 class="text-3xl md:text-4xl font-bold text-center mt-6 mb-2">
+			Listado de Hoteles
+		</h1>
+	</div>
+
+	<section class="grid grid-cols-2 gap-14 m-10 md:grid-cols-4">
+		<HotelCard
+			v-for="hotel in 20"
+			:key="hotel" />
+	</section>
 </template>
