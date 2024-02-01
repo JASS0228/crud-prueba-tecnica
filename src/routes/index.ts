419d7063b487c5d6d4dca1e7739a254966d69e50
JASS0228 <Salazarshome@outlook.com>
2024-01-31 20:47:42 -0600

    Se agrego una ruta de crear

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 
 import HotelsView from '../Views/HotelsView.vue'
+import CreateHotelView from '../Views/CreateHotelView.vue'
 
 const routes: RouteRecordRaw[] = [
 	{
 		path: '/',
 		name: 'Hotels',
 		component: HotelsView,
+	},
+	{
+		path: '/crear-hotel',
+		name: 'crear-hotel',
+		component: CreateHotelView,
 	},
 ]
 
