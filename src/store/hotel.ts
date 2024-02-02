31845fa963e00180870fa16eec244909c5fdf92f
JASS0228 <Salazarshome@outlook.com>
2024-02-02 15:14:23 -0600

    Se usa para tener en estado todos los hoteles que se haga al llamar la api

--- src/store/hotel.ts
+++ src/store/hotel.ts
@@ -1,10 +1,45 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { getHotels, getHotel } from '../api/index'
+import { HotelByID, Hotels } from '../types/hotelType'
 
 export const useHotelStore = defineStore('hotel', () => {
-	const hotels = ref([])
+	const hotels = ref<Hotels>()
+	const hotel = ref<HotelByID>()
+	const errors = ref()
+	const isLoading = ref(true)
+
+	// Se hace un fetch de los hoteles
+	async function fetchHotels() {
+		try {
+			isLoading.value = true
+			const { data } = await getHotels()
+			hotels.value = data
+		} catch (error) {
+			errors.value = error
+		} finally {
+			isLoading.value = false
+		}
+	}
+
+	async function fetchHotel(id: number) {
+		try {
+			isLoading.value = true
+			const { data } = await getHotel(id)
+			hotel.value = data
+		} catch (error) {
+			console.log(error)
+		} finally {
+			isLoading.value = false
+		}
+	}
 
 	return {
 		hotels,
+		hotel,
+		errors,
+		isLoading,
+		fetchHotels,
+		fetchHotel,
 	}
 })
