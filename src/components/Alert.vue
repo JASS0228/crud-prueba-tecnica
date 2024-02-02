ad30ff1fd4b044902c5123eb131e102127ed947a
JASS0228 <Salazarshome@outlook.com>
2024-02-02 15:13:11 -0600

    Se creo un componente alerta donde te muestra los campos si son requeridos o se completo

---
+++ src/components/Alert.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	const props = defineProps({
+		message: String,
+		type: Boolean,
+	})
+</script>
+
+<template>
+	<div
+		v-if="props.message"
+		:class="`${
+			props.type
+				? 'bg-red-100 border-red-500 text-red-700'
+				: 'bg-green-100 border-green-500 text-green-700'
+		} border-l-4 b p-4 mt-5 w-full md:w-1/3`"
+		role="alert">
+		<p class="font-bold">Error</p>
+		<p>{{ props.message }}</p>
+	</div>
+</template>
