f3c7286b1c42a3402b201941df5e91da777384d0
JASS0228 <Salazarshome@outlook.com>
2024-02-02 02:36:58 -0600

    Se agrego la vista de UpdateHotelView

---
+++ src/Views/UpdateHotelView.vue
@@ -0,0 +1,269 @@
+<script setup lang="ts">
+	import { computed, reactive } from 'vue'
+	import { useGetHotel, useUpdateHotel } from '../composables/useHotelQuery'
+	import { useRouter, useRoute } from 'vue-router'
+
+	const router = useRouter()
+	const route = useRoute()
+
+	const hotel = reactive({
+		name: '',
+		city: '',
+		rate: 0,
+		rating: 0,
+		currency: '',
+		description: '',
+		image: '',
+	})
+
+	const alert = reactive({
+		message: '',
+		type: false,
+	})
+
+	const { data } = useGetHotel(+route.params.id)
+	console.log(data.value)
+	Object.assign(hotel, data.value)
+
+	const { mutate } = useUpdateHotel()
+
+	//Función para resetear la alerta
+	const resetAlert = () => {
+		setTimeout(() => {
+			alert.message = ''
+			alert.type = false
+		}, 2000)
+	}
+
+	//Muestra una vista previa de la imagen
+	const imagePreview = computed(() => {
+		return hotel.image
+			? hotel.image
+			: 'https://placehold.co/700x400?text=Imagen+del+hotel'
+	})
+
+	//Función para manejar el envío del formulario
+	const handleSubmit = async () => {
+		const { city, currency, description, image, name, rate, rating } = hotel
+
+		// Validar que todos los campos estén llenos
+		if (
+			name === '' ||
+			city === '' ||
+			rate === 0 ||
+			rating === 0 ||
+			currency === '' ||
+			description === '' ||
+			image === ''
+		) {
+			alert.message = 'Todos los campos son requeridos'
+			alert.type = true
+			resetAlert()
+			return
+		}
+
+		// Validar que el precio y el rating sean números
+		if (isNaN(Number(rate)) || isNaN(Number(rating))) {
+			alert.message = 'El precio y el rating deben ser números'
+			alert.type = true
+			resetAlert()
+			return
+		}
+
+		//Validar que el numero no sea negativo o mayor del 1 al 5
+		if (Number(rating) < 1 || Number(rating) > 5) {
+			alert.message = 'El rating debe ser de 1 a 5 y no puede ser negativo'
+			alert.type = true
+			resetAlert()
+			return
+		}
+
+		//Validar que el precio no sea negativo o menor a 0
+		if (Number(rate) < 0 || Number(rate) === 0) {
+			alert.message = 'El precio no puede ser negativo o igual a 0'
+			alert.type = true
+			resetAlert()
+			return
+		}
+
+		// Validar que la moneda sea de 3 letras en mayúsculas
+		if (!/^[A-Z]{3}$/.test(currency)) {
+			alert.message = 'La moneda debe ser de 3 letras en mayúsculas'
+			alert.type = true
+			resetAlert()
+			return
+		}
+
+		// Enviar la datos a la api
+		mutate({
+			id: +route.params.id,
+			name,
+			city,
+			rate: rate.toString(),
+			rating: rating.toString(),
+			currency,
+			description,
+			image,
+		})
+
+		resetAlert()
+
+		// Limpiar el formulario
+		Object.assign(hotel, {
+			name: '',
+			city: '',
+			rate: 0,
+			rating: 0,
+			currency: '',
+			description: '',
+			image: '',
+		})
+
+		router.push({ name: 'Hotels' })
+	}
+</script>
+
+<template>
+	<div class="flex flex-col items-center justify-center">
+		<h1 class="text-3xl md:text-4xl font-bold text-center">Modificar Hotel</h1>
+
+		<div
+			v-if="alert.message"
+			:class="`${
+				alert.type
+					? 'bg-red-100 border-red-500 text-red-700'
+					: 'bg-green-100 border-green-500 text-green-700'
+			} border-l-4 b p-4 mt-5 w-full md:w-1/3`"
+			role="alert">
+			<p class="font-bold">Error</p>
+			<p>{{ alert.message }}</p>
+		</div>
+
+		<form
+			action=""
+			class="p-10 bg-white rounded-lg shadow-lg mt-3 w-full md:w-1/3"
+			@submit.prevent="handleSubmit()">
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+				<div>
+					<label
+						for="nombre"
+						class="text-lg font-bold block"
+						>Nombre</label
+					>
+					<input
+						id="nombre"
+						name="nombre"
+						type="text"
+						class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+						placeholder="Coloca el nombre del hotel"
+						v-model="hotel.name" />
+				</div>
+
+				<div>
+					<label
+						for="ciudad"
+						class="text-lg font-bold"
+						>Ciudad</label
+					>
+					<input
+						id="ciudad"
+						name="ciudad"
+						type="text"
+						class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+						placeholder="Coloca la ciudad del hotel"
+						v-model="hotel.city" />
+				</div>
+			</div>
+
+			<div></div>
+
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-3 mt-5">
+				<div>
+					<label
+						for="precio"
+						class="text-lg font-bold"
+						>Precio</label
+					>
+					<input
+						id="precio"
+						name="precio"
+						type="Number"
+						value="0"
+						class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+						v-model="hotel.rate" />
+				</div>
+
+				<div>
+					<label
+						for="rating"
+						class="text-lg font-bold"
+						>Rating</label
+					>
+					<input
+						id="rating"
+						name="rating"
+						type="Number"
+						value="0"
+						class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+						v-model="hotel.rating" />
+				</div>
+
+				<div>
+					<label
+						for="moneda"
+						class="text-lg font-bold"
+						>Moneda</label
+					>
+					<input
+						id="moneda"
+						name="moneda"
+						type="text"
+						class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+						placeholder="Moneda USD, MXN"
+						v-model="hotel.currency" />
+				</div>
+			</div>
+
+			<div class="mt-5">
+				<label
+					for="description"
+					class="text-lg font-bold block mb-1"
+					>Descripción</label
+				>
+				<textarea
+					name="description"
+					id="description"
+					cols="10"
+					rows="3"
+					class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+					placeholder="Coloca la descripción del hotel"
+					v-model="hotel.description"></textarea>
+			</div>
+
+			<div class="mt-5">
+				<img
+					:src="imagePreview"
+					:alt="hotel.name"
+					class="rounded-lg" />
+
+				<label
+					for="image"
+					class="text-lg font-bold block mb-1"
+					>Imagen</label
+				>
+				<input
+					type="text"
+					name="image"
+					id="image"
+					placeholder="Coloca la url de la imagen"
+					class="p-2 rounded-lg w-full placeholder:font-normal placeholder-gray-600 border-2 border-gray-300 focus:border-blue-500 focus:outline-none transition-all duration-300 ease-in-out"
+					v-model="hotel.image" />
+			</div>
+
+			<input
+				type="submit"
+				class="bg-green-500 mt-3 w-full p-2 rounded-lg text-white font-bold hover:bg-green-600 cursor-pointer transition-all"
+				value="Guardar Cambios" />
+		</form>
+	</div>
+</template>
